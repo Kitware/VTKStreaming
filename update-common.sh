2b0aac816b50729055ad50f34a403b8cbce9921a
Brad King <brad.king@kitware.com>
2021-09-29 14:06:59 -0400

    update-common: fix typo in comment

--- update-common.sh
+++ update-common.sh
@@ -121,7 +121,7 @@ cd "$toplevel_dir"
 # Check for an empty destination directory on disk.  By checking on disk and
 # not in the repo it allows a library to be freshly re-inialized in a single
 # commit rather than first deleting the old copy in one commit and adding the
-# new copy in a seperate commit.
+# new copy in a separate commit.
 if [ ! -d "$(git rev-parse --show-toplevel)/$subtree" ]; then
     readonly basehash=""
 else
