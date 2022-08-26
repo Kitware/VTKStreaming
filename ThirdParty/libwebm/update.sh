1f03859263dcacc8e3e5216f1bd89d9e665209a9
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2022-08-26 11:11:29 -0400

    Remove unused files and extra newlines
    (cherry picked from commit 1f2928194496975cb9291524c1d24277505a54cd)

--- ThirdParty/libwebm/update.sh
+++ ThirdParty/libwebm/update.sh
@@ -8,11 +8,11 @@ readonly name="libwebm"
 readonly ownership="Libwebm Upstream <kwrobot@kitware.com>"
 readonly subtree="ThirdParty/$name/vtk$name"
 readonly repo="https://gitlab.kitware.com/third-party/libwebm.git"
-readonly tag="for/vtk-20220825-1.0.0.28"
+readonly tag="for/vtk-20220826-1.0.0.28"
 readonly paths="
 build
-common/*.cc
-common/*.h
+common/webmids.h
+common/vtk_libwebm_mangle.h
 mkvmuxer
 mkvparser
 
@@ -31,6 +31,8 @@ README.libwebm
 extract_source () {
     git_archive
     pushd "$extractdir/$name-reduced"
+    sed -i '$ { /^$/ d}' LICENSE.TXT
+    sed -i '$ { /^$/ d}' README.libwebm
     mv -v CMakeLists.vtk.txt CMakeLists.txt
     popd
 }
