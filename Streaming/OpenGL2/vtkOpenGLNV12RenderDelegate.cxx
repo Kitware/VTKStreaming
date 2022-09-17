4c33e9fc705d1e4f3eb6db22bfd4862f3edb4b68
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2022-09-16 21:38:02 -0400

    streaming render delegates save OpenGL state before modifying state variables
    (cherry picked from commit e8ee316e358678a740244f19880daa095789fb31)

--- Streaming/OpenGL2/vtkOpenGLNV12RenderDelegate.cxx
+++ Streaming/OpenGL2/vtkOpenGLNV12RenderDelegate.cxx
@@ -76,27 +76,49 @@ void vtkOpenGLNV12RenderDelegate::Render(vtkTextureObject* nv12Texture,
 
   if (program != nullptr)
   {
+    const int width = window->GetActualSize()[0];
+    const int height = window->GetActualSize()[1];
+    float verts[] = { -1.0f, -1.0f, 0.0f, 1.0f, -1.0f, 0.0f, -1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f };
+    GLuint iboData[] = { 0, 1, 2, 2, 1, 3 };
+    bool startedWindowRender = false;
+
+    if (!window->CheckInRenderStatus())
+    {
+      window->Start();
+      startedWindowRender = true;
+    }
+
     shaderCache->ReadyShaderProgram(program);
     vtkOpenGLCheckErrors("Error readying shader program ");
-    float verts[] = { -1.0f, -1.0f, 0.0f, 1.0f, -1.0f, 0.0f, -1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f };
 
-    // bind and activate these textures before rendering that quad.
+    vtkOpenGLState* state = window->GetState();
+    vtkOpenGLState::ScopedglEnableDisable scissorTestSave(state, GL_SCISSOR_TEST);
+    state->vtkglDisable(GL_SCISSOR_TEST);
+
+    vtkOpenGLState::ScopedglEnableDisable depthTestSave(state, GL_DEPTH_TEST);
+    state->vtkglDisable(GL_DEPTH_TEST);
+
+    vtkOpenGLState::ScopedglEnableDisable blendSave(state, GL_BLEND);
+    state->vtkglDisable(GL_BLEND);
+
+    vtkOpenGLState::ScopedglViewport vportSave(state);
+    state->vtkglViewport(0, 0, width, height);
+
+    // bind and activate the texture before rendering that quad.
+    vtkOpenGLState::ScopedglActiveTexture textureSave(state);
     nv12Texture->Activate();
     program->SetUniform1iv("strides", 3, strides);
     program->SetUniform1iv("windowDims", 2, window->GetSize());
     program->SetUniformi("nv12Texture", nv12Texture->GetTextureUnit());
     program->SetUniformi("chromaHeight", chromaHeight);
-    GLuint iboData[] = { 0, 1, 2, 2, 1, 3 };
-
-    window->Start();
-    vtkOpenGLState* state = window->GetState();
-    state->vtkglDisable(GL_SCISSOR_TEST);
-    state->vtkglDisable(GL_DEPTH_TEST);
-
     vtkOpenGLRenderUtilities::RenderTriangles(
       verts, 4, iboData, 6, nullptr, program, this->DrawHelper.VAO);
-    window->End();
-    window->Frame();
     nv12Texture->Deactivate();
+
+    if (startedWindowRender)
+    {
+      window->End();
+      window->Frame();
+    }
   }
 }
--- Streaming/OpenGL2/vtkOpenGLRGBA32RenderDelegate.cxx
+++ Streaming/OpenGL2/vtkOpenGLRGBA32RenderDelegate.cxx
@@ -91,25 +91,47 @@ void vtkOpenGLRGBA32RenderDelegate::Render(
 
   if (program != nullptr)
   {
+    const int width = window->GetActualSize()[0];
+    const int height = window->GetActualSize()[1];
+    float verts[] = { -1.0f, -1.0f, 0.0f, 1.0f, -1.0f, 0.0f, -1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f };
+    GLuint iboData[] = { 0, 1, 2, 2, 1, 3 };
+    bool startedWindowRender = false;
+
+    if (!window->CheckInRenderStatus())
+    {
+      window->Start();
+      startedWindowRender = true;
+    }
+
     shaderCache->ReadyShaderProgram(program);
     vtkOpenGLCheckErrors("Error readying shader program ");
-    float verts[] = { -1.0f, -1.0f, 0.0f, 1.0f, -1.0f, 0.0f, -1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f };
 
-    // bind and activate these textures before rendering that quad.
+    vtkOpenGLState* state = window->GetState();
+    vtkOpenGLState::ScopedglEnableDisable scissorTestSave(state, GL_SCISSOR_TEST);
+    state->vtkglDisable(GL_SCISSOR_TEST);
+
+    vtkOpenGLState::ScopedglEnableDisable depthTestSave(state, GL_DEPTH_TEST);
+    state->vtkglDisable(GL_DEPTH_TEST);
+
+    vtkOpenGLState::ScopedglEnableDisable blendSave(state, GL_BLEND);
+    state->vtkglDisable(GL_BLEND);
+
+    vtkOpenGLState::ScopedglViewport vportSave(state);
+    state->vtkglViewport(0, 0, width, height);
+
+    // bind and activate the texture before rendering that quad.
+    vtkOpenGLState::ScopedglActiveTexture textureSave(state);
     rgba32Texture->Activate();
     program->SetUniformi("rgba32Texture", rgba32Texture->GetTextureUnit());
     program->SetUniformi("windowHeight", window->GetSize()[1]);
-    GLuint iboData[] = { 0, 1, 2, 2, 1, 3 };
-
-    window->Start();
-    vtkOpenGLState* state = window->GetState();
-    state->vtkglDisable(GL_SCISSOR_TEST);
-    state->vtkglDisable(GL_DEPTH_TEST);
-
     vtkOpenGLRenderUtilities::RenderTriangles(
       verts, 4, iboData, 6, nullptr, program, this->DrawHelper.VAO);
-    window->End();
-    window->Frame();
     rgba32Texture->Deactivate();
+
+    if (startedWindowRender)
+    {
+      window->End();
+      window->Frame();
+    }
   }
 }
