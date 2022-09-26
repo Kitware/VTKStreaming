95ca2e8cfa444e905f8bba58e8ce653aefbcd6a4
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2022-09-26 15:05:06 -0400

    adding git lfs: configure hooks for lfs

--- .hooks-config.bash
+++ .hooks-config.bash
@@ -1,6 +1,8 @@
 # Loaded by .git/hooks/(pre-commit|commit-msg|prepare-commit-msg)
 # during git commit after local hooks have been installed.
 
-hooks_chain_pre_commit="Utilities/Scripts/pre-commit"
-hooks_chain_commit_msg="Utilities/Scripts/commit-msg"
 hooks_chain_prepare_commit_msg="Utilities/Scripts/prepare-commit-msg"
+hooks_chain_post_checkout="Utilities/Scripts/post-checkout"
+hooks_chain_post_commit="Utilities/Scripts/post-commit"
+hooks_chain_post_merge="Utilities/Scripts/post-merge"
+hooks_chain_pre_push="Utilities/Scripts/pre-push"
