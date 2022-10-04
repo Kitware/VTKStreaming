7ddf190298a715b344cca60b34be4f3161004bd6
Jaswant Panchumarti <jaswant.panchumarti@kitware.com>
2022-10-03 21:25:31 -0400

    vtkVideoEncoder: add SetInputPixelFormat(int) override

--- Streaming/Encode/vtkVideoEncoder.h
+++ Streaming/Encode/vtkVideoEncoder.h
@@ -166,6 +166,7 @@ public:
    */
   vtkSetEnumMacro(InputPixelFormat, VTKPixelFormatType);
   vtkGetEnumMacro(InputPixelFormat, VTKPixelFormatType);
+  void SetInputPixelFormat(int pixFmt);
   ///@}
 
   ///@{
