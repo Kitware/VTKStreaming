24c0e920802ff41f0776206b42ce3845d837ac2e
Alexy Pellegrini <alexy.pellegrini@kitware.com>
2026-04-27 11:19:06 +0200

    Add smoke test for wheel

---
+++ tests/test_import.py
@@ -0,0 +1,8 @@
+def test_import():
+    """
+    This is a simple test that import the package and checks that it is found.
+    We can't really test more as runners do not have a NVidia GPU.
+    """
+    from vtk_streaming.vtkStreamingCore import vtkCompressedVideoPacket # noqa
+    from vtk_streaming.vtkStreamingOpenGL2 import vtkOpenGLVideoFrame # noqa
+    from vtk_streaming.vtkStreamingNvEncode import vtkNvEncoderGL # noqa
